2a5f411f64b9ebb2132ab7843143715c12dbbf95
AdrianCM21 <adriancasco2002@gmail.com>
2022-11-09 15:28:17 -0300

    Prueba Nombre de usuario

--- static/script.js
+++ static/script.js
@@ -3,4 +3,4 @@ btn.addEventListener('click',()=>{
   let numeroRandom=(Math.floor(Math.random()*6))+1;
     btn.style.backgroundImage='url(/static/img/L'+numeroRandom+'.png)';     
   console.log(numeroRandom);
-})
+}) 
