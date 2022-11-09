f555964411fe0aed0db3b4d2793876eca951d271
Adrian <adriancasco788@gmail.com>
2022-11-09 14:34:32 -0300

    c

--- static/script.js
+++ static/script.js
@@ -1,3 +1,6 @@
-document.getElementById('btn').addEventListener('click',()=>{
-    document.getElementById('dado').style.display='none';
+const btn=document.getElementById('btn');
+btn.addEventListener('click',()=>{
+  let numeroRandom=(Math.floor(Math.random()*6))+1;
+    btn.style.backgroundImage='url(/static/img/L'+numeroRandom+'.png)';     
+  console.log(numeroRandom);
 })
